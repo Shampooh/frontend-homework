a71f6c5a161f6881218b8b53d7d61c8c2a587a3d
Shampooh <agent.serega@yandex.ru>
2018-02-09 20:09:13 +0300

    Delete lot of +

---
+++ chess.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+
+const isPosInt = n => n | 0 == n && n > 1;
+
+const chess = num => {
+	if (!isNumeric(num) || !isPosInt(num)) {
+		return null; 
+	}
+	let res = "";
+	let baseStr = ("* ").repeat(num / 2 + 1)
+	for(let row = 0; row < num; row++) { 
+		res += baseStr.substring(row % 2, +num + row % 2) +"\n"; 
+	}
+	return res;
+};
+ 
